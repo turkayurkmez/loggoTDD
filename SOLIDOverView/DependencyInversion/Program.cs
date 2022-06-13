3f7972d36bea76db77067794e4cb19e6e5b0569f
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-06-13 21:42:54 +0300

    testable component via Dependency Inversion

--- SOLIDOverView/DependencyInversion/Program.cs
+++ SOLIDOverView/DependencyInversion/Program.cs
@@ -5,6 +5,10 @@ report.SendReport();
 
 Report report1 = new Report(new WSSender());
 report1.SendReport();
+
+
+Report report2 = new Report(new TestSender());
+report2.SendReport();
 public class Report
 {
     private ISender sender;
@@ -39,4 +43,13 @@ public class MailSender : ISender
         Console.WriteLine("mail gönderiliyor...");
     }
 }
+
+public class TestSender : ISender
+{
+    public void Send()
+    {
+        Console.WriteLine("Test amaçlı yazılmıştır");
+    }
+}
+
 
